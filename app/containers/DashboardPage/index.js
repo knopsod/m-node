cd43f5a418f84ee442131bcf582bcb4e47fe3eca
Noppadon Sodram <knopsod@gmail.com>
2018-07-23 06:01:58 +0700

    ok with recharts

--- app/containers/DashboardPage/index.js
+++ app/containers/DashboardPage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PieChart, Pie, Legend, Tooltip, Sector, Cell } from 'recharts';
 
 const incomePeriods = [
   { type: 'Daily', dollar: 999999, coin: 888888 },
@@ -7,8 +8,32 @@ const incomePeriods = [
   { type: 'Yearly', dollar: 999999, coin: 888888 },
 ];
 
+const data01 = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200},
+                  {name: 'Group E', value: 278}, {name: 'Group F', value: 189}]
+
+const data02 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
+                  {name: 'Group C', value: 1398}, {name: 'Group D', value: 9800},
+                  {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];
+
+const data = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+              {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;                    
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+ 	const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+ 
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
+    	{`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
 const index = props => {
-  
   return (
     <div>
       <h1>DASHBOARD</h1>
@@ -20,6 +45,25 @@ const index = props => {
       </ul>
       <h5>Masternode Status</h5>
       <h5>Report</h5>
+
+      <PieChart width={800} height={400}>
+        <Pie isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label/>
+        <Pie data={data02} cx={400} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
+        <Pie
+          data={data} 
+          cx={600} 
+          cy={200} 
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80} 
+          fill="#8884d8"
+        >
+        	{
+          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          }
+        </Pie>
+        <Tooltip/>
+       </PieChart>
 
     </div>)
 };
